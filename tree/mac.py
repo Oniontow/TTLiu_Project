63a889e5576b53bdf6f3dc8ef2f162bcdd26870f
王仁軒 <b11901027@ntu.edu.tw>
2024-11-18 17:47:02 +0800

    32 16

--- tree/mac.py
+++ tree/mac.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+VDD = 1.8
+PERIOD = 1.3
 prob = 0.9
 length = 50
-size = 16
+size = 4
 f16= "FFFFFFFFFFFFFFFF"
 f8 = "FFFFFFFF"
 f4 = "FFFF"
@@ -32,7 +34,7 @@ for i in range(length):
     for j in range(size*4):
         temp.append(array[j][i])
     array_t.append(temp)
-    result.append(str(hex(np.sum(temp)))[2:].upper())
+    result.append(str(hex(np.sum(temp)))[2:].upper().zfill(2))
 
 
 for i in range(length):
@@ -43,6 +45,56 @@ for i in range(length):
 # print (result)
 
 with open('out.txt', 'w') as file:
+    if(size == 16):
+        file.write(";add \" .VEC 'input.vec' \" to your .sp file to include the vector file\n")
+        file.write(";Vector Pattern\n")
+        file.write("Radix 4444444444444444" + "\n")
+        file.write("\t+4444444444444444\n")
+        file.write("\t+24\n")
+        file.write("vname in<[63:60]> in<[59:56]> in<[55:52]> in<[51:48]> in<[47:44]> in<[43:40]> in<[39:36]> in<[35:32]>\n \
+        +in<[31:28]> in<[27:24]> in<[23:20]> in<[19:16]> in<[15:12]> in<[11:8]> in<[7:4]> in<[3:0]>\n \
+        +in2<[63:60]> in2<[59:56]> in2<[55:52]> in2<[51:48]> in2<[47:44]> in2<[43:40]> in2<[39:36]> in2<[35:32]>\n \
+        +in2<[31:28]> in2<[27:24]> in2<[23:20]> in2<[19:16]> in2<[15:12]> in2<[11:8]> in2<[7:4]> in2<[3:0]>\n \
+        +result<[5:4]> result<[3:0]>\n")
+        file.write("io    iiiiiiiiiiiiiiii\n \
+        +iiiiiiiiiiiiiiii\n \
+        +oo\n\n")
+    elif(size == 8):
+        file.write(";add \" .VEC 'input.vec' \" to your .sp file to include the vector file\n")
+        file.write(";Vector Pattern\n")
+        file.write("Radix 44444444" + "\n")
+        file.write("\t+44444444\n")
+        file.write("\t+24\n")
+        file.write("vname in<[31:28]> in<[27:24]> in<[23:20]> in<[19:16]> in<[15:12]> in<[11:8]> in<[7:4]> in<[3:0]>\n \
+        +in2<[31:28]> in2<[27:24]> in2<[23:20]> in2<[19:16]> in2<[15:12]> in2<[11:8]> in2<[7:4]> in2<[3:0]>\n \
+        +result<[5:4]> result<[3:0]>\n")
+        file.write("io    iiiiiiii\n \
+        +iiiiiiii\n \
+        +oo\n\n")
+    elif(size == 4):
+        file.write(";add \" .VEC 'input.vec' \" to your .sp file to include the vector file\n")
+        file.write(";Vector Pattern\n")
+        file.write("Radix 4444" + "\n")
+        file.write("\t+4444\n")
+        file.write("\t+14\n")
+        file.write("vname in<[15:12]> in<[11:8]> in<[7:4]> in<[3:0]>\n \
+        +in2<[15:12]> in2<[11:8]> in2<[7:4]> in2<[3:0]>\n \
+        +result<[4:4]> result<[3:0]>\n")
+        file.write("io    iiii\n \
+        +iiii\n \
+        +oo\n\n")
+        
+    file.write("Tunit ns\n")
+    file.write("Period " + str(PERIOD) + "\n")
+    file.write("Odelay 0.6\n")
+    file.write("Trise 0.1\n")
+    file.write("Tfall 0.1\n")
+    file.write("VIH " + str(VDD) + "\n")
+    file.write("VIL 0\n")
+    file.write("VOH " + str(VDD) + "\n")
+    file.write("VOL 0\n\n")
+    
+    
     for i in range(length):
         if(size == 16):
             file.write(input_hex[i] + ' ' + f16 + ' ' + result[i] + '\n' )
